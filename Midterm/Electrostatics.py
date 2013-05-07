63517a628a10e9f34e5db3f0a615fa57990dc6dd
Alex Little <alexlittle222@gmail.com>
2013-05-07 14:04:29 -0700

    "Midterm"

--- Midterm/Electrostatics.py
+++ Midterm/Electrostatics.py
@@ -11,3 +11,12 @@ def dipolePotential(x,y,q1,d):
     k = 8.9875518*10**9
     Vxy = (k*q1/(x**2 + (y - d)**2)**(1/2.) - (k*q1/(x**2 + (y + d)**2)**(1/2.)))
     return Vxy
+
+
+def pointField(x,y,q,Xq,Yq):
+    """E is for the Ex value and W is the Ey value"""
+    k = 8.9875518*10**9
+    E = (k*q(x-Xq))/((x-Xq)**2 + (y-Yq)**2)**0.5
+    W = (k*q(y-Yq))/((x-Xq)**2 + (y-Yq)**2)**0.5
+    print E
+    print W
