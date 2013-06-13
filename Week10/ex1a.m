e83238c77eccf4207f27bcb84ca44b013fe20e50
aalittlephys202 <alexlittle222@gmail.com>
2013-06-12 21:14:47 -0700

    Finish Matlab Hw

--- Week10/ex1a.m
+++ Week10/ex1a.m
@@ -4,7 +4,11 @@ clear all; close all;
 x = [-10 -9 -8 -7 -6 -5 -4 -3 0];
 y = [2.65 2.10 1.90 1.40 1.00 0.80 0.60 0.30 0.00];
 ey = [0.1 0.1 0.1 0.1 0.05 0.05 0.05 0.05 0.2];
-% Plot the data with error bars 
+% Plot the data with error bars
+uncer = ey.^-2;
+[m,b,slerr,interr] = WeigthedLSQFit(x,y,uncer)
+figure(1)
+plot(x, m.*x + b)
 figure(1)
 errorbar(x,y,ey,'b.')
 % Don’t forget the labels
@@ -13,9 +17,24 @@ ylabel('y (mm)')
 axis equal
 %%
 hold on
-% Do something else now that the first part works.
+% Do something else now that the first part works
 hold off
 %%
 % Do something in a second figure window.
-%figure(5)
-%plot(x,x.^2)
+% function to fit 
+fun = @(a,b,c,x) -sqrt(a^2-(x-b).^2)+c;
+% Find a starting point for the parameters a, b, and c.
+guess = fun(15,0,15,x); % fun(a,b,c,x)
+figure(2)
+plot(x,guess,'r:')
+% fit the data
+fittedmodel = fit(x',y',fun,'StartPoint',[15 0 15])
+% plot the result
+plot(fittedmodel,'r-');
+% fit the data using the uncertainties as weights
+w = ey.^-2
+weightedfitted = fit(x',y',fun,'StartPoint',[15 0 15],'Weights',w')
+% plot the result
+plot(weightedfitted,'b-');
+figure(5)
+plot(x,x.^2)
